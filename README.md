1401032f65e9e8a22aea931299ffa3e6bfa6b80e
GTLangseth <grant.langseth@gmail.com>
2018-07-12 07:36:46 -0400

    Refactor function names

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Operating-Time-Deltas
+# Operating-Time-Deltas
 
 The following project sprang out of my company's need to measure the exact number of seconds between two points in time less the number of seconds that the business was not in operation. This would allow the company to determine the exact about of time certain items in its workflow had been pending during which there were individuals able to work on said task and therefore and calculate the associated turn around time on these items.
 
@@ -6,5 +6,9 @@ The basic formula for determine the number of operating seconds, O<sub>s</sub>, 
 
 > O<sub>s</sub> = (T<sub>2</sub> - T<sub>1</sub>) - W(T<sub>1</sub>, T<sub>2</sub>) - H(T<sub>1</sub>, T<sub>2</sub>) - N(T<sub>1</sub>, T<sub>2</sub>)
 
-Where W(T<sub>1</sub>, T<sub>2</sub>) represents the number of weekend elapsed seconds between the two points in time, H(T<sub>1</sub>, T<sub>2</sub>) represents the number of holiday based seconds between the two points in time, and N(T<sub>1</sub>, T<sub>2</sub>) represents the number of non-operation hours that occur on normal days of business [12am - 9am) and [5pm - 12am) if the business is operating [9am - 5pm).
+Where W(T<sub>1</sub>, T<sub>2</sub>) represents the number of weekend elapsed seconds between the two points in time, H(T<sub>1</sub>, T<sub>2</sub>) represents the number of holiday based seconds between the two points in time, and N(T<sub>1</sub>, T<sub>2</sub>) represents the number of non-operation hours that occur on normal days of business. For example, the time spans of [12am - 9am) and [5pm - 12am) if the business is operating [9am - 5pm).
+
+This module was made to be as flexible as possible and allows the user to set his or her own holidays when calculating total amount of business time elapsed. It takes advantage of some of the core functionality of `Pandas` and its timeseries modules in order to perform most of the computation.
+
+## Class Initialization
 
--- bTime.py
+++ bTime.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 import pandas as pd
 import datetime as dt
@@ -10,7 +11,7 @@ from pandas.tseries.holiday import (get_calendar, HolidayCalendarFactory,
 
 class operating_time(object):
     
-    def __init__(self,bt_start=9,bt_close=17,exclusion_list=[]):
+    def __init__(self, bt_start=9, bt_close=17, exclusion_list=[]):
         self.bt_start = bt_start
         self.bt_close = bt_close
         self.bt_len = self.bt_close - self.bt_start
@@ -24,19 +25,22 @@ class operating_time(object):
             Holiday('USIndependenceDay', month=7, day=4, observance=nearest_workday),
             USLaborDay,
             USThanksgivingDay,
-            Holiday('Christmas', month=12, day=25, observance=nearest_workday)
-        ]
+            Holiday('Christmas', month=12, day=25, observance=nearest_workday)]
+
         class CustomCal(AbstractHolidayCalendar):
             rules = self.holidays
+
         self.cal = get_calendar('CustomCal')
-        self.bday_us = cbd(calendar=self.cal)
+        self.bday = cbd(calendar=self.cal)
+
         
     def list_holidays(self,as_string=False):
         holiday_name_list = [i.name for i in self.holidays]
         if as_string:
-            for e,i in enumerate(holiday_name_list):
+            for e, i in enumerate(holiday_name_list):
                 print '{0}. {1}'.format(e,i)
         return holiday_name_list
+
     
     def remove_holidays(self,exclusion_list=[]):
         for i in exclusion_list:
@@ -49,43 +53,49 @@ class operating_time(object):
         class CustomCal(AbstractHolidayCalendar):
             rules = self.holidays
         self.cal = get_calendar('CustomCal')
-        self.bday_us = cbd(calendar=self.cal)
+        self.bday = cbd(calendar=self.cal)
+
     
     def list_us_bd(self,x, y):
-        return pd.DatetimeIndex(start=x.date(), end=y.date(), freq=self.bday_us)
+        return pd.DatetimeIndex(start=x.date(), end=y.date(), freq=self.bday)
 
-    def us_bd(self, x, y):
+
+    def count_bdays(self, x, y):
         try:
-            val = len(pd.DatetimeIndex(start=x.date(), end=y.date(), freq=self.bday_us))
+            val = len(pd.DatetimeIndex(start=x.date(), end=y.date(), freq=self.bday))
             return val
         except:
             return None
+
     
-    def is_b(self,x):
-        if self.us_bd(x,x) == 1:
+    def is_bday(self,x):
+        if self.count_bdays(x, x) == 1:
             return True
         else:
             return False
+
     
-    def hr_in(self,x):
+    def hr_in(self, x):
         hrIn = 0
-        if self.is_b(x) and x.hour >= self.bt_start:
+        if self.is_bday(x) and x.hour >= self.bt_start:
             if x.hour >= self.bt_close:
                 hrIn = self.bt_len
             else:
                 hrIn = (x.hour * 3600 + x.minute * 60 + x.second) / 3600.0 - self.bt_start
         return hrIn
 
-    def hr_out(self,x):
+
+    def hr_out(self, x):
         hrOut = 0
-        if self.is_b(x) and x.hour < self.bt_close:
+        if self.is_bday(x) and x.hour < self.bt_close:
             if x.hour < self.bt_start:
                 hrOut = self.bt_len
             else:
                 hrOut = self.bt_len - self.hr_in(x)
         return hrOut
 
-    def us_bdays(self,a,b):
+
+    def bdays(self, a, b):
         try:
             if pd.isnull(a) or pd.isnull(b):
                 return None
@@ -93,7 +103,7 @@ class operating_time(object):
                 x, y = b, a
             else:
                 x, y = a, b
-            bd_val = self.us_bd(x, y)
+            bd_val = self.count_bdays(x, y)
             bds = bd_val - (self.hr_in(x) + self.hr_out(y)) / float(self.bt_len)
             if a > b:
                 bds = 0 - bds
@@ -101,7 +111,8 @@ class operating_time(object):
         except:
             return None
         
-    def us_btime(self,a,b):
+
+    def btime(self, a, b):
         try:
             val = self.us_bdays(a,b)
             val = dt.timedelta(days=int(val),hours=(val-int(val))*self.bt_len)
